9dbcd36da37c153959fe44ff66e4f27f603a5a46
gaurav <gauravgupta11n@gmail.com>
2023-09-30 17:28:35 +0530

    add

---
+++ Task/module/add_sub.js
@@ -0,0 +1,9 @@
+function sum(a, b){
+    return a + b
+}
+
+function sub(c, d){
+    return c - d
+}
+
+module.exports = {sum, sub}
--- Task/module/index.js
+++ Task/module/index.js
@@ -1,8 +1,16 @@
-const obj = require("./mul_div")
-console.log("obj:", obj)
+const obj1 =require("./add_sub")
+const obj2 = require("./mul_div")
+console.log("obj1:", obj1)
+console.log("obj2:", obj2)
 
-let x = obj?.mul?.(5, 2)
+let z = obj1?.sum?.(100, 50)
+console.log("z:", z);
+
+let w = obj1?.sub?.(100, 30)
+console.log("w", w);
+
+let x = obj2?.mul?.(5, 2)
 console.log("x:", x)
 
-let y = obj?.div?.(20, 5)
+let y = obj2?.div?.(20, 5)
 console.log("y:", y)
