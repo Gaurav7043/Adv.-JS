311481df880d947f30fb9cb91ee5c36f24bc952b
gaurav <gauravgupta11n@gmail.com>
2023-10-05 16:06:39 +0530

    add

--- 28_call_back.js
+++ 28_call_back.js
@@ -15,22 +15,29 @@
 // fun2() // call karvana kahata hai
 
 // call back hell means 2 fun se jada fun hota hai tab use aata hai (function inherit)
-function fun1(arrg, cb){
+function fun1(arrgfun1, argsfun2, cb){
     setTimeout(()=>{
         console.log("------->fun1");
-        cb(arrg)
+        cb(arrgfun1, argsfun2)
     }, 2000)
 }
 
-function fun2(cb2){
+function fun2(cb2, argsfun2){
     setTimeout(()=>{
         console.log("------>fun2");
-        cb2()
+        cb2(argsfun2)
     }, 3000)
 }
 
-function fun3(){
-    console.log("------>fun3");
+function fun3(cb3){
+    setTimeout(()=>{
+        console.log("------>fun3");
+        cb3()
+    }, 2000)
 }
 
-fun1(fun3, fun2)
+function fun4(){
+    console.log("------>fun4");
+}
+
+fun1(fun3, fun4, fun2)
